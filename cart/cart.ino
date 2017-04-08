ed3aed61f5361c805fada10546977ae6c7b512bb
samr28 <samroq@yahoo.com>
2017-04-08 18:41:02 -0500

    Dart testing code

--- cart/cart.ino
+++ cart/cart.ino
@@ -1,19 +1,38 @@
 #include <Adafruit_TiCoServo.h>
 
+Adafruit_TiCoServo victor;
+
 void setup() {
-  Adafruit_TiCoServo dart;
-  dart.attach(9);
+  pinMode(9, OUTPUT);
+  victor.attach(9, 1500, 2000);
 
   Serial.begin(9600);
   Serial.println("Cart Starting!");
 }
 
 void loop() {
+    victor.write(0);
+    Serial.println("Set 0");
+    delay(1000);
+    victor.write(180);
+    Serial.println("Set 180");
+    delay(1000);
   if (Serial.available()) {
     char ch = Serial.read();
 
-    if (!isNan(ch)) {
-      dart.write(ch);
+    if (!isnan(ch)) {
+      if (ch == 1) {
+        victor.write(0);
+      }
+      else if (ch == 2) {
+        victor.write(90);
+      }
+      else if (ch == "3") {
+        Serial.print("Setting 180 ");
+        victor.write(180);
+      }
+      //victor.write(ch);
+      Serial.println(ch);
     }
   }
 }
